ef3c0d07490736a3bd209dfc22fcca0f0fc030fc
fkwa <kitsuruko@gmail.com>
2020-05-02 12:25:19 +0300

    Update according to new spec

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ function clean(): void
 
 function fixSession(object &$session): void
 {
-    $doc = json_decode(http("GET", "https://naurok.com.ua/api/test/session/" . $session->settings->document_id));
+    $doc = json_decode(http("GET", "https://naurok.com.ua/api/test/session/" . $session->session->id));
     if(!$doc)
         abort("Could not fetch answers");
     
